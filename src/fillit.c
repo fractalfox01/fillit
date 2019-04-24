ea1465b454d40cf483d6070f9981fb01aa0c11c8
Thomas Vandivier <tvandivi@e1z2r8p29.42.us.org>
2019-04-23 22:27:49 -0700

    backing up

--- src/fillit.c
+++ src/fillit.c
@@ -6,7 +6,7 @@
 /*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 18:35:26 by tvandivi          #+#    #+#             */
-/*   Updated: 2019/04/23 16:30:37 by tvandivi         ###   ########.fr       */
+/*   Updated: 2019/04/23 22:27:07 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,42 +37,6 @@ int		is_valid_char(char c)
 	return (0);
 }
 
-void	print_board(t_board *main_board)
-{
-	int	i;
-
-	i = 0;
-	while (i < main_board->tetra_count)
-	{
-		ft_putstr(main_board->solved_board[i++]);
-	}
-}
-
-void	print_pieces(t_board *mst)
-{
-	int		i;
-	t_piece	*tmp;
-	t_piece	*nxt;
-
-	i = 0;
-	tmp = mst->tmp_board;
-	while (tmp)
-	{
-		if (!(tmp->piece))
-			break ;
-		nxt = tmp->next;
-		while (i < 4)
-		{
-			ft_putstr(tmp->piece[i++]);
-			ft_putchar('\n');
-		}
-		ft_putnbr(tmp->p_num);
-		ft_putchar('\n');
-		i = 0;
-		tmp = nxt;
-	}
-}
-
 t_board	*new_board(int size)
 {
 	t_board new_board;
@@ -85,7 +49,6 @@ t_board	*new_board(int size)
 	ret = &new_board;
 	blank_board = (char *)malloc(sizeof(char) * 546);
 	ft_bzero(blank_board, 545);
-	// needs re-write to be a board to place pieces on. 5 x 5, 7 x 7, etc..
 	while (i < size)
 	{
 		tmp = ft_strnew(size + 1);
@@ -103,19 +66,16 @@ t_board	*new_board(int size)
 	return (ret);
 }
 
-int		read_file(char *file, t_board *n_board)
+int		read_file(char *file, t_board *n_board, int a)
 {
 	int		fd;
-	int		a;
-	t_piece	*tmp;
-	t_piece	*nxt;
-	char	*buf;
 	int		i;
+	t_piece	*tmp;
+	char	*buf;
 	
 	fd = open(file, O_RDONLY);
-	a = 0;
-	i = 0;
 	buf = ft_strnew(21);
+	i = 0;
 	if (fd > 0)
 	{
 		n_board->tmp_board = (t_piece *)malloc(sizeof(t_piece) * 1);
@@ -123,164 +83,15 @@ int		read_file(char *file, t_board *n_board)
 		while ((a = read(fd, buf, 21)) > 0)
 		{
 			if (a == 20 || a == 21)
-			{
-				tmp->next = (t_piece *)malloc(sizeof(t_piece) * 1);
-				nxt = tmp->next;
-				tmp->piece = &*ft_strsplit(buf, '\n');
-				tmp->p_num = i++;
-				tmp = nxt;
-				ft_bzero(buf, a);
-			}
+				tmp = add_lst_piece(tmp, buf, i, a);
 			else
 				return (0);
+			i++;
 		}
-/*		if ((a = read(fd, buf, 546)) > 0 && a < 546)
-		{
-			n_board = new_board(((a * .1) / 4));
-			n_board->tetri_count = ((a * .1) / 4);
-			store_board(buf, a, n_board);
-		}
-*/	}
+	}
 	if (!(!(n_board->tmp_board)))
 		return (1);
 	return (0);
-}
-
-void	set_length(t_board *main_board)
-{
-	t_piece	*tmp;
-	t_piece	*nxt;
-	int		i;
-
-	i = -1;
-	tmp = main_board->tmp_board;
-	while (tmp)
-	{
-		nxt = tmp->next;
-		i++;
-		tmp = nxt;
-	}
-	main_board->tetra_count = i;
-}
-
-int	c_chk(int k, int i, int j, char **block)
-{
-	int	count;
-
-	count = 0;
-	if (k % 4 != 0)
-	{
-		k++;
-		if (ft_isalpha(block[i][j]) == 1)
-			count++;
-	}
-	if (i >= 0 && i % 4 != 3)
-	{
-		if (ft_isalpha(block[i--][j]) == 1)
-			count++;
-	}
-	return (count);
-}
-
-int	check_vertical(int i, int j, char **block)
-{
-	int	count;
-	int	b;
-	int	k;
-	int dir;
-
-	count = 0;
-	b = 0;
-	k = 0;
-	if (i % 4 == 0 || i % 4 == 3)
-	{
-		if (i % 4 == 0)
-			dir = 1;
-		else
-			dir = -1;
-		b = i + 4;
-		i += dir;
-		if (is_valid_char(block[i][j]) == 2 && i < b)
-			count++;
-		return (count);
-	}
-	k = i;
-	return (c_chk(k, i, j, block));
-}
-
-int	check_horizonal(int i, int j, char **block)
-{
-	int count = 0;
-/*
-** range limits 0 - 103
-**start index is 0, 4, 8, 12
-*/
-	if (j == 0)
-	{
-		if (is_valid_char(block[i][j + 1]) == 2)
-			return (1);
-		else
-			return (0);
-	}
-	if (j == 3)
-	{
-		if (is_valid_char(block[i][j - 1]) == 2)
-			return (1);
-		else
-			return (0);
-	}
-	if (is_valid_char(block[i][j - 1]) == 2)
-		count++;
-	if (is_valid_char(block[i][j + 1]) == 2)
-		count++;
-	return (count);
-}
-
-int		verify_tetra(char **tab, int i, int j, int hash)
-{
-	int	count;
-
-	count = 0;
-	while (++i < 4)
-	{
-		while (++j < 4)
-		{
-			if (tab[i][j] == '.' || tab[i][j] == '#')
-			{
-				if (tab[i][j] == '#')
-				{
-					count += check_vertical(i, j, tab);
-					count += check_horizonal(i, j, tab);
-					hash++;
-				}
-			}
-			else
-				return (0);
-		}
-		j = -1;
-	}
-	if ((count == 6 || count == 8) && hash == 4)
-		return (1);
-	return (0);
-}
-
-int		verify_file(t_board *main_board)
-{
-	t_piece *tmp;
-	t_piece *nxt;
-	int		i;
-
-	i = 0;
-	tmp = main_board->tmp_board;
-	while (tmp)
-	{
-		nxt = tmp->next;
-		if (verify_tetra(tmp->piece, -1, -1, 0) == 0)
-			return (0);
-		ft_putstr("Tetra is good\n");
-		tmp = nxt;
-	}
-	return (1);
 }
 
 void	make_board(t_board *main_board)
@@ -298,14 +109,18 @@ void	fillit(char *file)
 	main_board = (t_board *)malloc(sizeof(t_board) * 1);
 	if (file)
 	{
-		if(read_file(file, main_board) == 1)
+		if(read_file(file, main_board, 0) == 1)
 		{
 			set_length(main_board);
-			verify_file(main_board);
-			ft_putnbr(main_board->tetra_count);
-			ft_putstr("\n");
+			if ((i = verify_file(main_board)) > 1)
+			{
+				ft_putstr("Bad block at: ");
+				ft_putnbr(i - 2);
+				ft_putchar('\n');
+				return ;
+			}
 		//	make_board(main_board);
-		//	print_pieces(main_board);
+			print_pieces(main_board);
 		//	print_board(main_board);
 		}
 		else
